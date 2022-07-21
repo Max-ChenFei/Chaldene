ad64b40315c93640ec44cc48cea9db17d7b05502
Duarte David <duarte@asus-debian.lan>
2022-07-21 07:56:19 -0500

    Limits context menu from exceeding the canvas width. Fixes #6

--- notebook/static/components/litegraph/litegraph.js
+++ notebook/static/components/litegraph/litegraph.js
@@ -13669,6 +13669,7 @@ LGraphNode.prototype.executeAction = function(action)
         //compute best position
         var left = options.left || 0;
         var top = options.top || 0;
+
         if (options.event) {
             left = options.event.clientX - 10;
             top = options.event.clientY - 10;
@@ -13683,9 +13684,18 @@ LGraphNode.prototype.executeAction = function(action)
 
             var body_rect = document.body.getBoundingClientRect();
             var root_rect = root.getBoundingClientRect();
-			if(body_rect.height == 0)
+			
+            var canvas = LGraphCanvas.active_canvas.canvas;
+            //make sure the contxt menu doesn't go to the right of the canvas
+            body_rect.width = canvas.getBoundingClientRect().right - body_rect.left;
+            
+            //the following line instead constrains the context menu to not go past either
+            //the bottom or the right
+            //body_rect = canvas.getBoundingClientRect()
+            
+            if(body_rect.height == 0)
 				console.error("document.body height is 0. That is dangerous, set html,body { height: 100%; }");
-
+            
             if (body_rect.width && left > body_rect.width - root_rect.width - 10) {
                 left = body_rect.width - root_rect.width - 10;
             }
