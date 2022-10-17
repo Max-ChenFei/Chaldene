da62ad87077f298867302cf2a8e3047e18ace8c1
Fei Chen <boxchenfei@gmail.com>
2022-10-17 23:32:17 +0200

    Add computeExecutionOrder function used when arrange (fix #70)

--- notebook/static/components/litegraph/litegraph.js
+++ notebook/static/components/litegraph/litegraph.js
@@ -982,6 +982,144 @@
         this.list_of_graphcanvas.splice(pos, 1);
     };
 
+     //This is more internal, it computes the executable nodes in order and returns it
+    LGraph.prototype.computeExecutionOrder = function(only_onExecute, set_level) {
+        var L = [];
+        var S = [];
+        var M = {};
+        var visited_links = {}; //to avoid repeating links
+        var remaining_links = {}; //to a
+
+        //search for the nodes without inputs (starting nodes)
+        for (var i = 0, l = this._nodes.length; i < l; ++i) {
+            var node = this._nodes[i];
+            if (only_onExecute && !node.onExecute) {
+                continue;
+            }
+
+            M[node.id] = node; //add to pending nodes
+
+            var num = 0; //num of input connections
+            if (node.inputs) {
+                for (var j = 0, l2 = node.inputs.length; j < l2; j++) {
+                    if (node.inputs[j] && node.inputs[j].link != null) {
+                        num += 1;
+                    }
+                }
+            }
+
+            if (num == 0) {
+                //is a starting node
+                S.push(node);
+                if (set_level) {
+                    node._level = 1;
+                }
+            } //num of input links
+            else {
+                if (set_level) {
+                    node._level = 0;
+                }
+                remaining_links[node.id] = num;
+            }
+        }
+
+        while (true) {
+            if (S.length == 0) {
+                break;
+            }
+
+            //get an starting node
+            var node = S.shift();
+            L.push(node); //add to ordered list
+            delete M[node.id]; //remove from the pending nodes
+
+            if (!node.outputs) {
+                continue;
+            }
+
+            //for every output
+            for (var i = 0; i < node.outputs.length; i++) {
+                var output = node.outputs[i];
+                //not connected
+                if (
+                    output == null ||
+                    output.links == null ||
+                    output.links.length == 0
+                ) {
+                    continue;
+                }
+
+                //for every connection
+                for (var j = 0; j < output.links.length; j++) {
+                    var link_id = output.links[j];
+                    var link = this.links[link_id];
+                    if (!link) {
+                        continue;
+                    }
+
+                    //already visited link (ignore it)
+                    if (visited_links[link.id]) {
+                        continue;
+                    }
+
+                    var target_node = this.getNodeById(link.target_id);
+                    if (target_node == null) {
+                        visited_links[link.id] = true;
+                        continue;
+                    }
+
+                    if (
+                        set_level &&
+                        (!target_node._level ||
+                            target_node._level <= node._level)
+                    ) {
+                        target_node._level = node._level + 1;
+                    }
+
+                    visited_links[link.id] = true; //mark as visited
+                    remaining_links[target_node.id] -= 1; //reduce the number of links remaining
+                    if (remaining_links[target_node.id] == 0) {
+                        S.push(target_node);
+                    } //if no more links, then add to starters array
+                }
+            }
+        }
+
+        //the remaining ones (loops)
+        for (var i in M) {
+            L.push(M[i]);
+        }
+
+        if (L.length != this._nodes.length && LiteGraph.debug) {
+            console.warn("something went wrong, nodes missing");
+        }
+
+        var l = L.length;
+
+        //save order number in the node
+        for (var i = 0; i < l; ++i) {
+            L[i].order = i;
+        }
+
+        //sort now by priority
+        L = L.sort(function(A, B) {
+            var Ap = A.constructor.priority || A.priority || 0;
+            var Bp = B.constructor.priority || B.priority || 0;
+            if (Ap == Bp) {
+                //if same priority, sort by order
+                return A.order - B.order;
+            }
+            return Ap - Bp; //sort by priority
+        });
+
+        //save order number in the node, again...
+        for (var i = 0; i < l; ++i) {
+            L[i].order = i;
+        }
+
+        return L;
+    };
+
     /**
      * Positions every node in a more readable manner
      * @method arrange
