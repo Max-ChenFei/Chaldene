d92b51f1477c6188fcd4ca75d17946fa7eea9efc
Duarte David <duda01@dfki.de>
2022-12-02 11:18:46 +0100

    Added search context menu

--- gui/lumino_test/src/index.js
+++ gui/lumino_test/src/index.js
@@ -312,6 +312,308 @@ define(['@lumino/commands', '@lumino/widgets'], function (
     }
   }
 
+  function _getWindowData(){
+    return {
+      pageXOffset: window.pageXOffset,
+      pageYOffset: window.pageYOffset,
+      clientWidth: document.documentElement.clientWidth,
+      clientHeight: document.documentElement.clientHeight
+    };
+  }
+
+  class SearchMenu extends Widget {
+  constructor(graph){
+      let node = document.createElement('div');
+
+      super({ node: node });
+      node.classList.add('SearchMenu');
+      let input = document.createElement('input');
+      let that = this;
+      this._search = null;
+      function createLabel(name,search_results){
+        let s = "";
+        s+=name.slice(0,search_results[0].index);
+        for(let i = 0;i<search_results.length-1;i++){
+          s+="<b>" +search_results[i][0] + "</b>";
+          s+=name.slice(search_results[i].index + search_results[i][0].length,search_results[i+1].index);
+        }
+        let i = search_results.length-1;
+        s+="<b>" +search_results[i][0] + "</b>";
+        s+=name.slice(search_results[i].index + search_results[i][0].length);
+        return s;
+      }
+      function updateSearch(){
+        if(!input.value){
+          that._search = null;
+          that.update();
+          return;
+        }
+
+        that._search = {};
+
+        for (const [category, group] of Object.entries(that._groups)){
+          let s1 = [...category.matchAll(input.value)];
+          let added = false;
+          if(s1.length>0){
+            that._search[category] = {name: createLabel(category, s1), list:[]}
+            added = true;
+          }
+
+          for(let i = 0; i<group.list.length; i++){
+            let member = group.list[i];
+            let s2 = [...member.matchAll(input.value)];
+            if(s2.length>0){
+              if(!added){
+                that._search[category] = {name: category, list:[]}
+                added = true;
+              }
+              that._search[category].list.push(createLabel(member,s2));
+              that._search[category].show = true;
+            }
+
+          }
+
+        }
+        that.update();
+
+      }
+
+      input.addEventListener("input",function(){
+        updateSearch();
+      });
+      node.appendChild(input);
+      input.placeholder = "Search...";
+
+      this._list = document.createElement('div');
+      node.appendChild(this._list);
+      this._groups = {};
+
+      this.addMember = function(category,name){
+        if(!this._groups[category]){
+          this._groups[category] = {name: category,show:false, list:[]};
+        }
+        this._groups[category].list.push(name);
+      }
+      let prevObj = null;
+
+      function onclick(obj){
+        if(prevObj){
+          prevObj.classList.remove("selected");
+        }
+        prevObj = obj;
+        obj.classList.add("selected");
+      }
+
+      function groupClick(group){
+        group.show = !group.show;
+        that.update();
+      }
+
+      this.update = function(){
+        this._list.remove();
+        this._list = document.createElement('div');
+        this._list.classList.add("SearchList");
+        this._list.style.maxHeight = "10rem";
+        node.appendChild(this._list);
+        let help = this._groups;
+        if(this._search){
+          if(Object.keys(this._search).length === 0){
+            this._list.innerHTML = "No search results...";
+            return;
+          } else {
+            help = this._search;
+          }
+        }
+        for (const [category, group] of Object.entries(help)){
+          let icon= document.createElement('i');
+          icon.style.width = "1rem";
+          icon.classList.add("fa");
+          if(group.show)
+          icon.classList.add("fa-chevron-down");
+          else
+          icon.classList.add("fa-chevron-right");
+          icon.ariaHidden=true;
+          let catTitle = document.createElement('p');
+          catTitle.innerHTML = group.name;
+          let catItem = document.createElement('div');
+
+          catItem.appendChild(icon);
+          catItem.appendChild(catTitle);
+          catItem.classList.add('categoryName');
+          catItem.onclick = function(){groupClick(group);};
+
+
+          this._list.appendChild(catItem);
+          if(group.show){
+            for(let i = 0; i<group.list.length; i++){
+              let memberEl = document.createElement('p');
+              memberEl.innerHTML = group.list[i];
+              memberEl.classList.add('memberEl');
+              memberEl.onclick = function(){onclick(memberEl);};
+
+
+              this._list.appendChild(memberEl);
+            }
+          }
+        }
+      }
+
+
+      this.setFlag(Widget.Flag.DisallowLayout);
+      this.addClass('content');
+      this.title.label = 'Members of' + graph.name;
+      this.title.closable = true;
+      this.title.caption = 'Shows all the members in ' + graph.name;
+
+
+      let gs = mockLiteGraphGetRegisteredNodes();
+      for (const [category, group] of Object.entries(gs)){
+        for(let i = 0; i<group.length;i++){
+          this.addMember(category, group[i])
+        }
+      }
+
+
+      this.update();
+
+
+      this.addClass('lm-Menu');
+      this.onBeforeAttach = Menu.prototype.onBeforeAttach;
+      this.onAfterAttach = Menu.prototype.onAfterAttach;
+
+      this._evtKeyDown = function(event){
+
+      }
+
+      function hit(e){
+        let bb = node.getBoundingClientRect();
+
+        let x = e.clientX;
+        let y = e.clientY;
+
+        return x>= bb.left &&
+              x<= bb.left + bb.width &&
+              y>=bb.top &&
+              y<=bb.top+bb.height;
+
+      }
+
+      this._evtMouseDown = function(event){
+        if(!hit(event)){
+          this.close();
+        }
+      }
+
+
+      this._evtMouseUp = function(event){
+
+      }
+
+
+      this._evtMouseMove = function(event){
+
+      }
+
+      this._evtMouseEnter = function(event){
+
+      }
+
+      this._evtMouseLeave = function(event){
+
+      }
+
+      this.handleEvent = function(event){
+        switch (event.type) {
+          case 'keydown':
+            this._evtKeyDown(event);
+            break;
+          case 'mouseup':
+            this._evtMouseUp(event);
+            break;
+          case 'mousemove':
+            this._evtMouseMove(event);
+            break;
+          case 'mouseenter':
+            this._evtMouseEnter(event);
+            break;
+          case 'mouseleave':
+            this._evtMouseLeave(event);
+            break;
+          case 'mousedown':
+            this._evtMouseDown(event);
+            break;
+          case 'contextmenu':
+            event.preventDefault();
+            event.stopPropagation();
+            break;
+        }
+      }
+
+
+    }
+
+
+
+
+    close(){
+      if(this.isAttached){
+        Widget.detach(this);
+      }
+    }
+    open(x,y){
+      const windowData = _getWindowData();
+      let px = windowData.pageXOffset;
+      let py = windowData.pageYOffset;
+      let cw = windowData.clientWidth;
+      let ch = windowData.clientHeight;
+
+      let forceX = false;
+      let forceY = false;
+
+      // Compute the maximum allowed height for the menu.
+      let maxHeight = ch - (forceY ? y : 0);
+
+      // Fetch common variables.
+      let node = this.node;
+      let style = node.style;
+
+      // Clear the menu geometry and prepare it for measuring.
+      style.opacity = '0';
+      style.maxHeight = `${maxHeight}px`;
+      //style.maxHeight = "20rem";
+
+      // Attach the menu to the document.
+      Widget.attach(this, document.body);
+
+      // Measure the size of the menu.
+      let { width, height } = node.getBoundingClientRect();
+
+      // Adjust the X position of the menu to fit on-screen.
+      if (!forceX && x + width > px + cw) {
+        x = px + cw - width;
+      }
+
+      // Adjust the Y position of the menu to fit on-screen.
+      if (!forceY && y + height > py + ch) {
+        if (y > py + ch) {
+          y = py + ch - height;
+        } else {
+          y = y - height;
+        }
+      }
+
+      // Update the position of the menu to the computed position.
+      style.transform = `translate(${Math.max(0, x)}px, ${Math.max(0, y)}px`;
+
+      // Finally, make the menu visible on the screen.
+      style.opacity = '1';
+
+    }
+  }
+
+  function createSearchMenu(name){
+    return new SearchMenu(name)
+  }
 
   class GraphEditor extends Widget {
     constructor(graph) {
@@ -361,11 +663,15 @@ define(['@lumino/commands', '@lumino/widgets'], function (
       let that = this;
       this.ctx = ctx;
       this.graph = new LiteGraph.LGraph();
+      let serachMenu =createSearchMenu({name:"main"});
       node.addEventListener('contextmenu', function (event) {
         let cs = mockLiteGraphGetContextMenu(event);
-        if(cs){
-          let m = createMenu(cs);
+        if(cs.type === "ContextMenu"){
+          let m = createMenu(cs.items);
           m.open(event.clientX,event.clientY);
+        } else {
+          console.log("Search");
+          serachMenu.open(event.clientX,event.clientY);
         }
         event.preventDefault();
         event.stopPropagation();
@@ -414,9 +720,20 @@ define(['@lumino/commands', '@lumino/widgets'], function (
     ]
   }
 
+  function mockLiteGraphGetRegisteredNodes(){
+    let nodes = {};
+    nodes["Math"] = ["Add","Multiply","Divide"];
+    nodes["Display"] = ["Image","String"];
+    nodes["Color"] = ["RGB2BW", "HSV", "Contrast","Brightness"];
+    return nodes;
+  }
+
   function mockLiteGraphGetContextMenu(e /* MouseEvent */){
     //make sure to return null when the graph is being dragged
-    return [
+    if(false)
+    return {
+      type: "ContextMenu",
+      items: [
       {command: "add_node"},
       {command: "toggle_minimap"},
       {command: "node_properties", args:{name: "Add function", left: 23.1, right: 5.4}},
@@ -439,7 +756,11 @@ define(['@lumino/commands', '@lumino/widgets'], function (
           ]
         }
       }
-    ]
+    ]};
+
+    return {
+      type: "NodeSearch"
+    };
   }
 
   function main(){
